545386330d4fcc0fa3119017f076ea558d1d5e01
yukii5 <assault03@gmail.com>
2020-11-06 17:10:27 +0900

    文章一部変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# 5.times do |n|
+#   username  = "example-#{n+1}"
+#   email = "example-#{n+1}@example.com"
+#   password = "password"
+#   User.create!(username:  username,
+#                email: email,
+#                password:              password,
+#                password_confirmation: password)
+# end
+# 100.times do |n|
+#   family_name  = "example-#{n+1}"
+#   first_name  = "example-#{n+1}"
+#   family_name_kana  = "example-#{n+1}"
+#   first_name_kana  = "example-#{n+1}"
+#   age  = "example-#{n+1}"
+#   conpany  = "example-#{n+1}"
+#   position  = "example-#{n+1}"
+  
+#   Searches.create!(
+#     family_name:  family_name,
+#     first_name: first_name,
+#     family_name_kana: family_name_kana,
+#     first_name_kana: first_name_kana,
+#     sex_id: sex_id,
+#     age: age,
+#     conpany: conpany,
+#     position: position
+#   )
+# end
+
